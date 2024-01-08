09ce000c408c5d9e42500e30fd9677ddd6101646
Belodri <33916281+Belodri@users.noreply.github.com>
2024-01-08 13:54:44 +0100

    Update SpaceMod.lua
    Added "add-bio-to-ftl" and "add-military-to-ftl" setting options

--- SeaBlock/settings-updates/SpaceMod.lua
+++ SeaBlock/settings-updates/SpaceMod.lua
@@ -1,4 +1,20 @@
 -- Space Extension Mod
+
+data:extend({
+  {
+    type = "bool-setting",
+    name = "add-bio-to-ftl",
+    setting_type = "startup",
+    default_value = false,
+  },
+  {
+    type = "bool-setting",
+    name = "add-military-to-ftl",
+    setting_type = "startup",
+    default_value = false,
+  },
+})
+
 if mods["SpaceMod"] then
   seablock.overwrite_setting("bool-setting", "SpaceX-no-bob", false)
   seablock.set_setting_default_value("bool-setting", "SpaceX-ignore-tech-multiplier", true)
